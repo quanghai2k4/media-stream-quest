040e3b66bf63cb21a6dc6b59a903061ddfbbf06a
Dylan <117840980+quanghai2k4@users.noreply.github.com>
2025-08-06 11:12:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "media-stream-quest.onrender.com",
     port: 8080,
   },
   plugins: [
